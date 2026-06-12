6de8ec1b47236fb71587442cc3c68cad247beaff
reimondez154002 <reimondez.154002@e.unavarra.es>
2026-06-12 21:11:24 +0200

    [green] - Pasa test que comprueba si devuelve cuenta a 0 si esta vacia

--- src/Order.php
+++ src/Order.php
@@ -1,6 +1,18 @@
 <?php
 
-class Order{
+class Order {
+    private Menu $menu;
+    public function __construct(Menu $menu) {
+        $this->menu = $menu;
+    }
 
+    public function handle(string $instruction): string {
+        $command = strtolower(trim($instruction));
+
+        if ($command === "cuenta") {
+            return "Total: 0.00";
+        }
+
+        return "";
+    }
 }
-
