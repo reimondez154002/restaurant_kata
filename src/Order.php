aa253a707bf9be47f8e041e8fc76d4146239ac25
reimondez154002 <reimondez.154002@e.unavarra.es>
2026-06-12 21:43:24 +0200

    [refactor] - Extraer comando añadir a funcion privada

--- src/Order.php
+++ src/Order.php
@@ -11,8 +11,7 @@ class Order {
     public function handle(string $instruction): string {
         $parts = explode(" ", trim($instruction));
         $command = strtolower($parts[0] ?? "");
-        $dish = strtolower($parts[1] ?? "");
-        $quantity = strtolower($parts[2] ?? 1);
+
 
         if ($command === "cuenta") {
 
@@ -21,19 +20,26 @@ class Order {
 
         if ($command === "añadir") {
 
-            $price = $this->menu->getPrice($dish);
-
-            if ($price === null) {
-                return "El plato seleccionado no existe en el menú";
-            }
-
-            $this->items[$dish] = 1;
-            $totalPrice = $price * $quantity;
-            $totalFormatted = number_format($totalPrice, 2, '.', '');
-
-            return "{$dish} x{$quantity} | Total: {$totalFormatted}";
+            return $this->addItem($parts);
         }
 
         return "";
     }
+     private function addItem(array $parts):string{
+
+        $dish = strtolower($parts[1] ?? "");
+        $quantity = isset($parts[2]) ? (int)$parts[2] : 1;
+
+        $price = $this->menu->getPrice($dish);
+
+        if ($price === null) {
+            return "El plato seleccionado no existe en el menú";
+        }
+
+        $this->items[$dish] = $quantity;
+        $totalPrice = $price * $quantity;
+        $totalFormatted = number_format($totalPrice, 2, '.', '');
+
+        return "{$dish} x{$quantity} | Total: {$totalFormatted}";
+     }
 }
